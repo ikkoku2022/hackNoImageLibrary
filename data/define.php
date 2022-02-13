3efc3810376fe228bd2369c0ddfc0160fdbd3c22
ikkoku2022 <KillerPantherhageregere+github@gmail.com>
2022-02-14 00:17:18 +0900

    モザイク処理ライブラリなし

---
+++ data/define.php
@@ -0,0 +1,12 @@
+<?php
+    //共通宣言
+
+    // ディレクトリの場所
+    define("ROOT_DIR", "/hogehoge/");
+
+    //モザイク処理する画像のファイルパス
+    define('MATERIAL', ROOT_DIR.'data/sample.jpg');
+
+    //モザイク処理した画像の出力時のファイルパス
+    define('CONVERT_MATERIAL', ROOT_DIR.'data/sample2.jpg');
+?>
